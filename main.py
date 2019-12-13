f6db17a5010cc1702a20b0dd06b727e969dd0067
writetonikhil <writetonikhil@gmail.com>
2019-12-13 12:53:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     Log = setup_custom_logger('parking')
     Log.info("Starting Parking app...")
 
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
